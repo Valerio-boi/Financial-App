9a3da99d25ad9e2520c3aaefe1842e5255bd7340
Valerio boi <boivalerio@gmail.com>
2025-03-26 22:00:07 +0100

    Inizio transazioni

---
+++ banking-client/src/app/components/button-transaction/button-transaction.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button-transaction',
+  imports: [],
+  templateUrl: './button-transaction.component.html',
+  styleUrl: './button-transaction.component.css'
+})
+export class ButtonTransactionComponent {
+  @Input() label!: string; 
+}
---
+++ banking-client/src/app/components/transaction/transaction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonTransactionComponent } from "../button-transaction/button-transaction.component";
+
+@Component({
+  selector: 'app-transaction',
+  imports: [ButtonTransactionComponent],
+  templateUrl: './transaction.component.html',
+  styleUrl: './transaction.component.css'
+})
+export class TransactionComponent {
+
+}
