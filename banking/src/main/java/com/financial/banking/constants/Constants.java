632c29dacb6675139e6a0ddbcad3a43bf9909aa3
Valerio boi <boivalerio@gmail.com>
2025-03-28 17:10:35 +0100

    sonarQube

--- banking/src/main/java/com/financial/banking/constants/Constants.java
+++ banking/src/main/java/com/financial/banking/constants/Constants.java
@@ -2,6 +2,8 @@ package com.financial.banking.constants;
 
 public class Constants {
 
+    private Constants() {throw new IllegalStateException("Constants class");}
+
     public static final String ERRORE_DATA_BASE = "Si é verificato un errore interno, riprovare!!!";
 
 }
